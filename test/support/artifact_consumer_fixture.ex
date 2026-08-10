493258867e348b9150502f4f07c1220f6a967ece
szTheory <szTheory@users.noreply.github.com>
2026-08-10 17:38:19 -0400

    fix(96-05): track package fixture source compilation
    - Declare the package-owned fixture as an external compiler resource\n- Retain the priv fixture as the sole implementation source\n- Invalidate test support when the fixture changes

--- test/support/artifact_consumer_fixture.ex
+++ test/support/artifact_consumer_fixture.ex
@@ -1,1 +1,8 @@
-Code.require_file("../../priv/adoption_proof/artifact_consumer_fixture.ex", __DIR__)
+defmodule Chimeway.Test.ArtifactConsumerFixtureSource do
+  @fixture Path.expand("../../priv/adoption_proof/artifact_consumer_fixture.ex", __DIR__)
+  @external_resource @fixture
+
+  def fixture, do: @fixture
+end
+
+Code.require_file(Chimeway.Test.ArtifactConsumerFixtureSource.fixture())
