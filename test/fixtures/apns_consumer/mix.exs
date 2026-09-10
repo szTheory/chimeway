27784c165bc267e73cc2cca4079cca5fef5270c8
szTheory <szTheory@users.noreply.github.com>
2026-09-10 00:02:10 -0400

    deps: bump APNs consumer Mint to 1.10.0

--- test/fixtures/apns_consumer/mix.exs
+++ test/fixtures/apns_consumer/mix.exs
@@ -6,6 +6,7 @@ defmodule APNSConsumer.MixProject do
       app: :apns_consumer,
       version: "0.1.0",
       elixir: "~> 1.17",
+      lockfile: System.get_env("MIX_LOCKFILE") || "mix.lock",
       deps: deps()
     ]
   end
